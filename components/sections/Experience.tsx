efd7e6ca53870a260ccaaddd17ac785850b76959
DonnieDesu <ramirezdonpiedro@gmail.com>
2026-07-14 19:14:14 +0800

    Changes in sections

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import SectionContainer from '@/components/SectionContainer';
+import SectionContainer from '../components/SectionContainer';
 
 const Experience = () => {
   const experiences = [
